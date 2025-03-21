901c0828f9c5bac9dc7b6f019b558907f3714e69
LeGenda <151869637+LeGenda3107@users.noreply.github.com>
2025-03-21 16:35:09 +0200

    catalog

--- Site/pages/catalog.php
+++ Site/pages/catalog.php
@@ -9,29 +9,125 @@
 			<h3>Ціна</h3>
 			<div class="row">
 				<p>від</p>
-				<input type="" name="">
+				<input class="inputCatalog" placeholder="тут" type="number" name="text" >
 				<p>до</p>
-				<input type="" name="">
+				<input class="inputCatalog" placeholder="тут" type="number" name="text" >
 				<p>грн</p>
 			</div>
 		</div>
 		<div class="parameters">
-			<h3>Ціна</h3>
-			<div class="row">
-				<input type="checkbox">
-				<p>малий</p>
-			</div>
-			<div class="row">
-				<input type="checkbox">
-				<p>середній</p>
-			</div>
-			<div class="row">
-				<input type="checkbox">
-				<p>великий</p>
+			<h3 class="toggle-button">Розмір</h3>
+			<div class="parameters-content">
+			    <label class="row">
+					<input type="checkbox" class="input">
+					<span class="custom-checkbox"></span>
+					<p>малий</p>
+				</label>
+				<label class="row">
+					<input type="checkbox" class="input">
+					<span class="custom-checkbox"></span>
+					<p>середній</p>
+			    </label>
+			    <label class="row">
+					<input type="checkbox" class="input">
+					<span class="custom-checkbox"></span>
+					<p>великий</p>
+			    </label>
 			</div>
 		</div>
+		<div class="parameters">
+		  <h3 class="toggle-button">Колір</h3>
+		  <div class="parameters-content">
+		    <label class="row">
+		      <input type="checkbox" class="input">
+		      <span class="custom-checkbox"></span>
+		      <p>червоний</p>
+		    </label>
+		    <label class="row">
+		      <input type="checkbox" class="input">
+		      <span class="custom-checkbox"></span>
+		      <p>зелений</p>
+		    </label>
+		    <label class="row">
+		      <input type="checkbox" class="input">
+		      <span class="custom-checkbox"></span>
+		      <p>синій</p>
+		    </label>
+		  </div>
+		</div>
+		<div class="parameters">
+		  <h3 class="toggle-button">Матеріал</h3>
+		  <div class="parameters-content">
+		    <label class="row">
+		      <input type="checkbox" class="input">
+		      <span class="custom-checkbox"></span>
+		      <p>дерево</p>
+		    </label>
+		    <label class="row">
+		      <input type="checkbox" class="input">
+		      <span class="custom-checkbox"></span>
+		      <p>метал</p>
+		    </label>
+		    <label class="row">
+		      <input type="checkbox" class="input">
+		      <span class="custom-checkbox"></span>
+		      <p>пластик</p>
+		    </label>
+		  </div>
+		</div>
+		<script>
+			const toggleButtons = document.querySelectorAll('.toggle-button');
+
+			toggleButtons.forEach(button => {
+				button.addEventListener('click', () => {
+					const currentContent = button.nextElementSibling;
+					const isOpen = button.classList.contains('open');
+
+				 	if (isOpen) {
+						// Згортання поточного
+						currentContent.style.height = currentContent.scrollHeight + 'px';
+						requestAnimationFrame(() => {
+							currentContent.style.height = '0px';
+						});
+						button.classList.remove('open');
+					} else {
+						// Розгортання поточного
+						currentContent.style.height = currentContent.scrollHeight + 'px';
+						button.classList.add('open');
+						currentContent.addEventListener('transitionend', function handler() {
+							if (button.classList.contains('open')) {
+								currentContent.style.height = 'auto';
+							}
+							currentContent.removeEventListener('transitionend', handler);
+						});
+					}
+				});
+			});
+		</script>
 	</aside>
 	<article class="rightPanel">
+		<?php
+		for ($i = 0; $i < 30; $i++) {
+		    echo '
+		    <div class="card">
+		        <div class="textBox">
+		            <p class="text head">Ethereum</p>
+		            <span>Cryptocurrency</span>
+		            <p class="text price">1.654,34€</p>
+		        </div>
+		        <img src="../images/dwarf.png">
+		    </div>
+		    ';
+		}
+		?>
+		<div class="card">
+			<div class="textBox">
+				<p class="text head">Ethereum</p>
+				<span>Cryptocurrency</span>
+				<p class="text price">1.654,34€</p>
+			</div>
+			<img src="../images/dwarf.png">
+		</div>
 	</article>
 </section>
 <?php
---
+++ Site/pages/commodity.php
@@ -0,0 +1,18 @@
+<?php
+	$title = "commodity";
+	include '../blocks/header.php';
+?>
+<section class="container">
+	<article class="info">
+		
+	</article>
+	<article class="model">
+		
+	</article>
+	<article class="price">
+		
+	</article>
+</section>
+<?php
+	require '../blocks/footer.php';
+?>
