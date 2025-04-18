201c1f06d0b0d0a833d39173196a28095c80c609
nazar <nazar@gmai.com>
2025-04-18 17:35:35 +0300

    блять коміти кал всьо поудалялось(половину пофіксив))

--- Site/pages/index.php
+++ Site/pages/index.php
@@ -37,57 +37,93 @@
             <div class="subtitle">  -Ми надаємо можливість кожному з вас змоделювати і надрукувати ваше “хочу”. Завдяки (Наша назва) ви можете завантажити будь-яку вашу 3d модель, починаючи від гарної статуетки до промислової деталі.</div>
             <div class="left-sideImage"><img src=""></div>
             <div class="threeBlocks">
+            <div>
                 <div class="head">
-                    <div class="headTitle"></div>
+                <div class="headTitle">Наші послуги</div>
                 </div>
                 <div class="block">
-                    <div class="blockText"></div>
+                <div class="blockText">
+                        <p>-3D-моделювання на замовлення;</p>
+                        <p>-3D-візуалізація;</p>
+                        <p>-3D-друк.</p>
+                    </div>
                 </div>
-
+                </div>
+            <div>
                 <div class="head">
-                    <div class="headTitle"></div>
+                <div class="headTitle">Чому ми?</div>
                 </div>
                 <div class="block">
-                    <div class="blockText"></div>
+                <div class="blockText">
+                        <p>-Професіоналізм;</p>
+                        <p>-Швидкість;</p>
+                        <p>-Якість;</p>
+                        <p>-Доступність.</p>  
+                    </div>
                 </div>
-
+                </div>
+            <div>
                 <div class="head">
-                    <div class="headTitle"></div>
+                <div class="headTitle">Процес роботи</div>
                 </div>
                 <div class="block">
-                    <div class="blockText"></div>
+                <div class="blockText">
+                        <p>-Ви замовляєте в нас готову модельку або закидаєте свою власну модель.</p>
+                    <p>-Модель автоматично відправляється на принтер і починає процес друку.</p>
+                </div>
+                </div>
                 </div>
             </div>
+            <dic>
             <div class="ratingBlock">
                 <div class="head">
-                    <div class="headTitle"></div>
+                      
+                <div class="headTitle">Відгуки клієнтів</div>
                 </div>
-                <div class="rating"></div>
+                <div class="rating">
+                <p>-я сосав мене їбали якийсь даун видалив цей блок, а я забув, які відгуки тут були написані.</p>
+                <p>-я сосав мене їбали якийсь даун видалив цей блок, а я забув, які відгуки тут були написані.</p>
+                <p>-я сосав мене їбали якийсь даун видалив цей блок, а я забув, які відгуки тут були написані.</p> 
+                </div>
+            </div>
             </div>
         </div>
     </div>
-</section> 
-
-<section class="gallery">
-    <div class="container">
-      <div class="grayBlock">
-            <div class="title">Ось що у нас вже є</div>
+    <div class="grayblock">
+    <div class="title">Ось що у нас вже є</div>
             <div class="usb"></div>
             <div class="catalogButton">Каталог</div>
-</div>
+            </div>  
+            </div>
     </div>
 </section>
 <section class="moreInfoScreen">
     <div class="container">
         <div class="grayBlock">
-    <div class="left-sideImage"><img src=""></div>
-    <div class="infoTitle"></div>
-    <div class="infoText"></div>
-    <div class="infoText"></div>
-    <div class="megaButton">Штопаєм ділдака!</div>
+        <div class="left-sideImage"><img src=""></div>
+                <div class="leftP"></div>
+                <div class="rightP">
+                    <div class="infoTitle">Бля ну тут прям хуяка точений остався текста того, але я не знаю що написати</div>
+                    <div class="infoText">bla bla bla
+                        ble ble ble ble ble
+                        blu blu blu blu blu blu blu blu blu blu blu blu
+                        habl blu bli ble ble bla ble blu bla ble ble bli
+                        lingan guli guli guli watcha lin gan gu lin gan gu</div>
+                    <div class="infoText">bla bla bla
+                        ble ble ble ble ble
+                        blu blu blu blu blu blu blu blu blu blu blu blu
+                        habl blu bli ble ble bla ble blu bla ble ble bli
+                        lingan guli guli guli watcha lin gan gu lin gan gu</div>
+                    
+                </div>
+                <div class="megaButton">Штопаєм ділдака!</div>
+                
 <div class="right-sideImage"><img src=""></div>
 </div>
-</div>
+</div>    
+
+
+
 </section>
 
 <footer class="footer">
